634da8396f6a4732298571c5dcc7f4bb7fb24bc2
ccoloradoanselmo <ccoloradoanselmo.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 13:05:35 +0100

    Refactor select input for recipients to use dynamic data

--- agradecer.php
+++ agradecer.php
@@ -1,32 +1,96 @@
-<!DOCTYPE html>
+<?php
+require 'configdb.php';
+
+// Función de conexión
+function conectar(){
+    $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+    $conexion->set_charset("utf8"); 
+    return $conexion;
+}
+
+// Función para mostrar alumnos
+function mostrar_alumnos(){ 
+    $conexion = conectar();  
+
+
+    $sql = "SELECT * FROM alumnos";
+    $resultado = $conexion->query($sql);    
+
+    
+    //PRUEBA 2: repetir fetch_array() tantas veces como filas haya en la tabla alumnos
+    while($fila = $resultado->fetch_array()){
+        echo '<option value="'.$fila["idAlumno"].'">'.$fila["nombre"].' - '.$fila["idAlumno"].'</option>';
+    }
+
+
+
+
+
+    // PRUEBA 1: repetir 3 veces fetch_array
+    /*for($i = 0; $i < 3; $i++){
+        $fila = $resultado->fetch_array();
+
+        
+            echo "<p>";
+            echo "Nombre: " . $fila["nombre"];
+            echo "<br>";
+            echo "idAlumno: " . $fila["idAlumno"];
+            echo "<br>";
+            //echo "Edad: " . $fila["edad"];
+            
+            echo "</p>";
+        
+    }
+*/
+    $conexion->close();
+}
+
+?>
+
+<!DOCTYPE html class="body">
 <html class="body" lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mensaje</title>
     <link rel="stylesheet" href="estilos.css">
+    
+        <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body class="body">
+    <!-- HEADER -->
+    <header class="barra-superior">
 
+        <div class="logo">
+            <a href="index.html">
+                <img src="./Imagenes/logo-jesuitas.svg" alt="Jesuitas">
+            </a>
+        </div>
+
+        <nav class="menu">
+            <a href="./agradecer.html">Agradecer</a>
+            <a href="#">Recibir</a>
+            <a href="./login.html">Cerrar sesión</a>
+        </nav>
+
+    </header>
     <div class="contenedor-login">
         <form class="caja-login" action="#">
             <h2 class="titulo-login">Enviar mensaje</h2>
-
-            <!-- Campo Para: con select -->
-            <label for="para">Para:</label>
-            <select id="para" name="para"> 
-			<!-- Se hara con un bucle While o For, se pondran tantos options como alumnos haya en la tabla Alumnos -->
-			<!-- En el value, estara la variable que estara almacenando el nombre y apellido de cada alumno -->
-			<!-- Gracias al bucle, podremos hacerlo de manera dinamica -->
-			 <!-- <option value="$alumno">$alumno</option> -->
-                <option value="">Selecciona destinatario</option>
-                <option value="persona1">Persona 1</option>
-                <option value="persona2">Persona 2</option>
-                <option value="persona3">Persona 3</option>
+            <!-- Campo Para: con select --> 
+            <b><label class="titulo-izquierda" for="para">Para:</label></b>
+            <select class="select-izq" id="para" name="para">
+                <?php
+                    mostrar_alumnos();
+                ?>
             </select>
-
+            <br>
+            <br>
             <!-- Textarea -->
-            <label for="mensaje">Mensaje:</label>
+            <b><label class="titulo-izquierda" for="mensaje">Mensaje:</label></b>
+            <br>
+            <br>
             <textarea class="textarea" id="mensaje" name="mensaje" rows="6" placeholder="Escribe tu mensaje aquí..." ></textarea>
 
             <button type="submit">Enviar</button>
