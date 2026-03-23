24ed0860a3f0fe64e84bc9f119baa64fbcaec5b7
ccoloradoanselmo <ccoloradoanselmo.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 20:01:39 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,21 @@
 <?php
+
+require 'configdb.php';
+
 //Primero conectamos con la BD
+function conectar(){
+    $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+    $conexion->set_charset("utf8"); 
+    return $conexion;
+}
 
 //Antes de guardar nada, validamos que todo este correcto y que no pillemos correos o contraseñas invalidos (eso no lo hacemos todavia)
-$usuario = $_POST["usuario"];
+$nombre = $_POST["nombre"];
 
 $password = $_POST["password"];
-$sql = "SELECT idAlumno from ALUMNO WHERE usuario ='".$usuario."' AND password='".$password."'";
+
+
+$sql = "SELECT idAlumno from ALUMNO WHERE nombre ='".$nombre."' AND password='".$password."'";
 //Antes del query se hace siempre esto
 
 //TAREA mostrar la consulta SQL
